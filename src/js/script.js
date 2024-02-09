e736369e3781618178882049270b611c8dcf9ae8
Sachin <sachin333777@outlook.com>
2024-02-09 20:26:29 +0530

    fully responsive

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,41 @@
-const navToggle = document.querySelector('.nav-toggle');
-const links = document.querySelector('.links');
+const sendBtn = document.querySelector(".send__btn");
+// console.log(sendBtn);
+// const { EmailJSResponseStatus } = require("@emailjs/browser");
 
-navToggle.addEventListener('click', () => {
-  // console.log('link', links.classList);
-  // console.log('link', links.classList.contains('links'));
-  // if(links.classList.contains('show-links')) {
-  //   links.classList.remove('show-links');
-  // } else {
-  //   links.classList.add('show-links');
-  // }
+function sendMail() {
+  const serviceId = "service_w3bwktm";
+  const templateId = "template_9bftnty";
+  const params = {
+    name: document.querySelector("#name").value,
+    email: document.querySelector("#email").value,
+    message: document.querySelector("#message").value,
+  };
+  emailjs
+    .send(serviceId, templateId, params)
+    .then((res) => {
+      document.querySelector("#name").value = "";
+      document.querySelector("#email").value = "";
+      document.querySelector("#message").val = "";
+      console.log(res);
+      alert("message send successfully");
+    })
+    .catch((err) => console.log(err));
+}
 
-  links.classList.toggle('show-links');
-  navToggle.classList.toggle('active');
+sendBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  sendMail();
 });
 
+
+
+//     <script type="text/javascript"
+//         src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js">
+// </script>
+// <script type="text/javascript">
+//    (function(){
+//       emailjs.init({
+//         publicKey: "NE-Gxp61XZHJcAwG9",
+//       });
+//    })();
+// </script>
