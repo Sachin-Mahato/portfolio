9111cbe90463cba62530807a2c02122a2d3137af
sachin <sachin333777@outlook.com>
2024-11-21 09:00:35 +0530

    add new project e-commerce

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,11 @@
 "use strict"
-const submitButton = document.body.querySelector(".submit-btn");
-submitButton.addEventListener("click", () => {
-    alert(`Please note that this feature is temporarily disabled.Feel free to reach out to me through social media.`)
+document.addEventListener("DOMContentLoaded", () => {
+
+    const submitButton = document.body.querySelector(".submit-btn");
+    submitButton.addEventListener("click", () => {
+        alert(`Please note that this feature is temporarily disabled.Feel free to reach out to me through social media.`)
+    })
+
 })
 
 
