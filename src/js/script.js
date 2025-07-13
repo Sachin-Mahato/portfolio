65515b9899a627ade62a29942fb165da55634200
sachin <sachin333777@outlook.com>
2025-07-13 13:57:10 +0530

    chore: update html

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,5 @@
 "use strict"
-document.addEventListener("DOMContentLoaded", () => {
 
-    const submitButton = document.body.querySelector(".submit-btn");
-    submitButton.addEventListener("click", () => {
-        alert(`Please note that this feature is temporarily disabled.Feel free to reach out to me through social media.`)
-    })
-
-})
-
-
+    (
+        () => console.log("Hello there")
+    )();
